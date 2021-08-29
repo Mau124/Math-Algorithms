e8a93bb5d0de0f4a3d75b8ec076deb1468cf45b3
Mauricio Flores <andresperez1024@gmail.com>
2021-08-29 16:21:00 -0500

    Matrix classs added
    Operations added in matrix class:
    
    add: Matrix with Matrix
    subs: Matrix with Matrix
    mult: Matrix with scalar
    mult: Matrix with matrix

--- matrix.py
+++ matrix.py
@@ -1,17 +1,125 @@
-"""This file is for matrices operations
-
 """
-def dot_product(A, B):
-    ans = 0
-    for i in range(len(A)):
-        ans += (A[i]*B[i])
-    return ans
+    TODO:
+        Comments
+"""
+class Matrix:
 
-def add(A, B):
-    ans = []
-    for i in range(len(A)):
-        tmp = []
-        for j in range(len(A[0])):
-            tmp.append(A[i][j] + B[i][j])
-        ans.append(tmp)
+    def __init__(self, Aux):
+        """Constructor a Matrix class. It takes a list o lists as parameter"""
+        self.n = len(Aux)
+
+        if isinstance(Aux[0], list):
+            self.m = len(Aux[0])
+        else:
+            self.m = 1 
+
+        self.A = []
+        for i in range(self.n):
+            if isinstance(Aux[i], list):
+                self.A.append(Aux[i])
+            else:
+                self.A.append([Aux[i]])
+
+    def __add__(self, B):
+        """Matrix addition. Returns the result of A + B"""
+        if self.n != B.n or self.m != B.m:
+            raise ValueError('Matrices must have same dimensions')
+
+        C = []
+        for i in range(self.n):
+            tmp = []
+            for j in range(self.m):
+                tmp.append(self[i, j] + B[i, j])
+            C.append(tmp)
+
+        return Matrix(C)
+
+    def __sub__(self, B):
+        """Matrix substraction. Returns the result of A - B"""
+        if self.n != B.n or self.m != B.m:
+            raise ValueError('Matrices must have same dimensions')
+
+        C = []
+        for i in range(self.n):
+            tmp = []
+            for j in range(self.m):
+                tmp.append(self[i, j] - B[i, j])
+            C.append(tmp)
+
+        return Matrix(C)
+
+    def __mul__(self, c):
+        """Multiplication by scalar. Returns the result of c * A"""
+        if not (isinstance(c, int) or isinstance(c, float)):
+            raise ValueError('Multiplication is by an scalar')
+
+        C = []
+
+        for i in range(self.n):
+            tmp = []
+            for j in range(self.m):
+                tmp.append(self[i, j] * c)
+            C.append(tmp)
+            
+        return Matrix(C)
+
+    def __rmul__(self, c):
+        """Multiplication by scalar. Returns the result of c * A"""
+        return self.__mul__(c)
+
+    def __matmul__(self, B):
+        """Matrix multiplication. Returns the result of A * B"""
+        if self.m != B.n:
+            raise ValueError('Columns of first Matrix needs to match with rows of second Matrix')
+
+        C = []
+        for i in range(self.n):
+            tmp = []
+            for j in range(B.m):
+                aux = 0
+                for k in range(self.m):
+                    aux += (self[i,k]*B[k,j])
+                tmp.append(aux)
+            C.append(tmp)
+
+        return Matrix(C)
+
+
+    def __getitem__(self, index):
+        """Return the item that is in the position index"""
+        if isinstance(index, tuple):
+            i = index[0]
+            j = index[1]
+            return self.A[i][j]
+
+        if isinstance(index, int):
+            return self.A[index]
+
+    def __setitem__(self, index, value):
+        """Change the value of the item in position index with value"""
+        if isinstance(index, tuple):
+            i = index[0]
+            j = index[1]
+            self.A[i][j] = value 
+    
+    def __repr__(self):  
+        """Return a string with the representation of a matrix"""
+        mt_str = ''
+        for i in range(self.n):
+            mt_str += '|\t'
+            for j in range(self.m):
+                mt_str += str(self.A[i][j]) + '\t'
+            mt_str += '|\n'
+
+        return mt_str
+                
+    
+def dot_product(A, B):
+    """Return the dot product of Matrix A and B"""
+    if A.n != B.n:
+        raise ValueError('Matrices does not have same size')
+
+    ans = 0
+    for i in range(A.n):
+        ans += (A[i, 0] * B[i, 0])
     return ans
